c538c233b26ef31363b399c67e35e71f10ec6de5
Pryann <pryann@gmail.com>
2026-03-04 16:35:23 +0100

    change transactions

--- data/transaction/bank-transaction.js
+++ data/transaction/bank-transaction.js
@@ -1,23 +1,34 @@
+// ACID refers to a set of four properties—Atomicity, Consistency, Isolation, and Durability
+
 const session = db.getMongo().startSession()
 session.startTransaction()
 
 print('Tranzakció indítása...')
 
 try {
-  const accounts = session.getDatabase('bank').accounts
+    const accounts = session.getDatabase('bank').accounts
 
-  // Pénz kivétele az egyik számláról
-  accounts.updateOne({ _id: 'account1' }, { $inc: { balance: -100 } })
+    // Pénz kivétele az egyik számláról
+    accounts.updateOne({ _id: 'account1' }, { $inc: { balance: -100 } })
 
-  // Pénz hozzáadása a másik számlához
-  accounts.updateOne({ _id: 'account2' }, { $inc: { balance: 100 } })
+    // Pénz hozzáadása a másik számlához
+    accounts.updateOne({ _id: 'account2' }, { $inc: { balance: 100 } })
 
-  // Ha idáig eljutottunk, minden művelet sikeres volt
-  session.commitTransaction()
+    // Ha idáig eljutottunk, minden művelet sikeres volt
+    session.commitTransaction()
+    print('Tranzakció sikeres')
 } catch (error) {
-  // Hiba esetén minden változtatás visszavonása
-  session.abortTransaction()
-  print('Tranzakció hiba:', error)
+    // Hiba esetén minden változtatás visszavonása
+    session.abortTransaction()
+    print('Tranzakció hiba:', error)
 } finally {
-  session.endSession()
+    session.endSession()
 }
+
+// WIthout JS, not recommended for production, just for demonstration purposes:
+// session = db.getMongo().startSession()
+// session.startTransaction()
+// session.getDatabase('bank').accounts.updateOne({ _id: 'account1' }, { $inc: { balance: -100 } })
+// session.getDatabase('bank').accounts.updateOne({ _id: 'account2' }, { $inc: { balance: 100 } })
+// session.commitTransaction()
+// session.endSession()
